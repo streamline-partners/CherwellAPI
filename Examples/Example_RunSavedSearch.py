2b4073e25d6469068e4c7e66dd77ca46a81a7080
aaron.cohee@streamlinepartners.com.au <aaron.cohee@streamlinepartners.com.au>
2020-09-28 11:16:27 +1000

    removed os and sys paths

--- Examples/Example_RunSavedSearch.py
+++ Examples/Example_RunSavedSearch.py
@@ -1,8 +1,5 @@
 from __future__ import print_function
 import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
 from CherwellAPI import CherwellClient
 from CherwellAPI import CherwellCredentials
 import pickle
@@ -23,7 +20,7 @@ password = "Password123"
 api_key = "2940baca-1e3a-4f5e-863a-a4e2370a8633"
 
 #Create Encrypted Credentials
-# CherwellCredentials.create_encrypted_cherwell_credentials(password,api_key)
+CherwellCredentials.create_encrypted_cherwell_credentials(password,api_key)
 
 # Create a new Cherwellclient connection
 cherwell_client = CherwellClient.Connection(base_uri=base_uri, username=username)
